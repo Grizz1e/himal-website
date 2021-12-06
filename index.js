396a309788a4f03684a8abfd914f70b6350ae18c
Grizz1e <thejackshiva@gmail.com>
2021-12-06 11:13:42 +0545

    Some Changes have been made

--- index.js
+++ index.js
@@ -1,37 +1,8 @@
-const counters = document.querySelectorAll('.value');
-const speed = 400;
-
-counters.forEach(counter => {
-  const animate = () => {
-    const value = +counter.getAttribute('akhi');
-    const data = +counter.innerText;
-
-    const time = value / speed;
-    if (data < value) {
-      counter.innerText = Math.ceil(data + time);
-      setTimeout(animate, 1);
-    } else {
-      counter.innerText = value.toLocaleString() + "+";
-    }
-
-  }
-
-  animate();
-});
-
-
-
 document.addEventListener('DOMContentLoaded', () => {
-
-
   const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
-
   if ($navbarBurgers.length > 0) {
-
-
     $navbarBurgers.forEach(el => {
       el.addEventListener('click', () => {
-
         const target = el.dataset.target;
         const $target = document.getElementById(target);
         el.classList.toggle('is-active');
@@ -40,16 +11,11 @@ document.addEventListener('DOMContentLoaded', () => {
       });
     });
   }
-
 });
 
 $(document).ready(function () {
-
-
   $(".navbar-burger").click(function () {
-
     $(".navbar-burger").toggleClass("is-active");
     $(".navbar-menu").toggleClass("is-active");
-
   });
 });
