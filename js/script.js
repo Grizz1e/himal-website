c7de7141656df04e3d25348e53409d95b1bb054b
Grizz1e <thejackshiva@gmail.com>
2022-09-07 12:48:07 +0545

    Updated commands page

--- js/script.js
+++ js/script.js
@@ -3,6 +3,13 @@ const radioCount = document.getElementById('radio-count');
 const userCount = document.getElementById('user-count');
 const hamMenu = document.getElementById('ham-menu');
 
+const allBtn = document.getElementById('all-btn')
+const botBtn = document.getElementById('bot-btn')
+const musicBtn = document.getElementById('music-btn')
+const botCat = document.getElementById('bot-cat')
+const musicCat = document.getElementById('music-cat')
+const commands = document.querySelectorAll('.command-name')
+
 let sUpto = 0;
 let rUpto = 29700;
 let uUpto = 99700;
@@ -11,6 +18,7 @@ let uUpto = 99700;
 let counts = setInterval(counter);
 
 function counter() {
+    if (!serverCount) return
     serverCount.textContent = ++sUpto;
     radioCount.textContent = ++rUpto;
     userCount.textContent = ++uUpto;
@@ -27,3 +35,32 @@ function counter() {
 hamMenu.addEventListener('click', () => {
     document.getElementById('mobile-menu').classList.toggle('is-visible')
 })
+
+allBtn.addEventListener('click', () => {
+    botCat.classList.remove('hidden')
+    musicCat.classList.remove('hidden')
+    botBtn.classList.remove('bg-slate-500')
+    allBtn.classList.add('bg-slate-500')
+    musicBtn.classList.remove('bg-slate-500')
+})
+botBtn.addEventListener('click', () => {
+    botCat.classList.remove('hidden')
+    musicCat.classList.add('hidden')
+    botBtn.classList.add('bg-slate-500')
+    allBtn.classList.remove('bg-slate-500')
+    musicBtn.classList.remove('bg-slate-500')
+
+})
+musicBtn.addEventListener('click', () => {
+    botCat.classList.add('hidden')
+    musicCat.classList.remove('hidden')
+    botBtn.classList.remove('bg-slate-500')
+    allBtn.classList.remove('bg-slate-500')
+    musicBtn.classList.add('bg-slate-500')
+})
+
+commands.forEach(element => {
+    element.addEventListener('click', () => {
+        element.querySelector('.collapsible').classList.toggle('hidden')
+    })
+});
